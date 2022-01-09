59a6b23ae9c1ff21561cc34affa12708bbbd5f43
dbs-sticky <mark.evans@physics.org>
2022-01-09 08:21:12 +0000

    Created a new JS version

---
+++ scripts/why-i-prefer-the-kindle.js
@@ -0,0 +1,26 @@
+var dt = new Date();
+document.getElementById("datetime").innerHTML =
+  ("0" + dt.getHours()).slice(-2) + ":" + ("0" + dt.getMinutes()).slice(-2);
+
+var currentPage = 0;
+function goToPage(pageNumber) {
+  const main = document.getElementById("main_place");
+  const div = document.getElementById("section" + pageNumber);
+  const clone = div.cloneNode(true);
+  while (main.firstChild) main.firstChild.remove();
+  main.appendChild(clone);
+  currentPage = pageNumber;
+}
+
+function goNextPage() {
+  goToPage(currentPage + 1);
+}
+
+function goPreviousPage() {
+  goToPage(currentPage - 1);
+}
+
+function darkToggle() {
+  var element = document.body;
+  element.classList.toggle("dark-toggle");
+}
