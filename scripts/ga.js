492e9d2dbf149e35a93c17372682b629fd1ed563
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2022-03-06 09:23:23 +0000

    Finished the stacked bar chart post

--- scripts/ga.js
+++
@@ -1,7 +0,0 @@
-
-async src="https://www.googletagmanager.com/gtag/js?id=G-L464TSFFK9">
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-L464TSFFK9');
