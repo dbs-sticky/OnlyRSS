58b72cd6338a14e8680e6c1c3de3ce014d3a1146
mendhak <mendhak@users.noreply.github.com>
2023-06-18 14:31:34 +0100

    Refactor figures and headings outlines into a single method

--- scripts/outline.js
+++ scripts/outline.js
@@ -1,6 +1,6 @@
-function createOutline() {
+function createOutline(elementType, targetId) {
     // Get all the headings in the document
-    let headings = document.querySelectorAll("h2");
+    let headings = document.querySelectorAll(elementType);
 
     // Create an empty array to store the outline items
     let outline = [];
@@ -8,7 +8,7 @@ function createOutline() {
     // Process each heading and add to the outline array
     headings.forEach((heading, idx) => {
         text = heading.textContent;
-        id = `heading-${idx}`;
+        id = `${elementType}-${idx}`;
         // Set an ID for each heading
         heading.setAttribute("id", id);
         outline.push({text, id});
@@ -21,12 +21,22 @@ function createOutline() {
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.setAttribute("href", `#${o.id}`);
+        // Set title attribute, to help with long text
+        a.setAttribute("title", o.text.trim());
         a.textContent = o.text;
         li.appendChild(a);
         contentList.appendChild(li);
     });
   
   // Append the generated links to the outline div
-	document.getElementById("doc_outline").appendChild(contentList);
+	document.getElementById(targetId).appendChild(contentList);
   
+}
+
+function createHeadingsOutline(){
+  createOutline("h2", "doc_outline");
+}
+
+function createFiguresOutline(){
+  createOutline("figure", "doc_figures")
 }
