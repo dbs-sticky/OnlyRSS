d6a3b4343558c5587d3a89ba19b437c071e257eb
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2023-12-16 11:16:20 +0000

    Add styles for image popups

---
+++ scripts/web-components.js
@@ -0,0 +1,47 @@
+class DialogImage extends HTMLElement {
+	constructor() {
+		super();
+	}
+	
+	connectedCallback() {
+		// Get my image, should be one only.
+		let img = this.querySelector('img');
+    console.log(img);
+
+		if(!img) {
+			console.warn('dialog-image: No image found. Exiting.');
+			return;
+		}
+
+		let parent = img.parentNode;
+		if(parent.nodeName !== 'A') {
+			console.warn('dialog-image: Image not wrapped in link. Exiting.');
+			return;			
+		}
+
+		let fullImageLink = parent.href;
+		
+		let dialog = document.createElement('dialog');
+		
+		dialog.innerHTML = `
+  <form method="dialog">
+		<p>
+		<img src="${fullImageLink}" loading="lazy">
+		</p>
+		<p style="text-align:center">
+    <button type="submit">Close</button>
+		</p>
+  </form>
+		`;
+		parent.parentNode.insertBefore(dialog, parent.nextSibling);
+
+		img.addEventListener('click', e => {
+			e.preventDefault();
+			dialog.showModal();
+		});
+		
+	}
+
+}
+
+if(!customElements.get('dialog-image')) customElements.define('dialog-image', DialogImage);
