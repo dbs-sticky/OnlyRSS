11f6d667b58b51ff51f73a62df8e12d22adaf805
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2023-12-18 14:16:45 +0000

    Refactor image gallery and dialog component

--- scripts/web-components.js
+++ scripts/web-components.js
@@ -5,43 +5,48 @@ class DialogImage extends HTMLElement {
 
 
 	
-	connectedCallback() {
-		// Get my image, should be one only.
-		let img = this.querySelector('img');
+  connectedCallback() {
+    // Get my image, should be one only.
+    let img = this.querySelector('img');
 
-		if(!img) {
-			console.warn('dialog-image: No image found. Exiting.');
-			return;
-		}
+    if(!img) {
+        console.warn('dialog-image: No image found. Exiting.');
+        return;
+    }
 
-		let parent = img.parentNode;
-		if(parent.nodeName !== 'A') {
-			console.warn('dialog-image: Image not wrapped in link. Exiting.');
-			return;			
-		}
+    let parent = img.parentNode;
+    if(parent.nodeName !== 'A') {
+        console.warn('dialog-image: Image not wrapped in link. Exiting.');
+        return;            
+    }
 
-		let fullImageLink = parent.href;
-		let dialog = document.createElement('dialog');
+    let fullImageLink = parent.href;
+    let dialog = document.createElement('dialog');
     let description = (img.getAttribute('title') || '');  // Get the title attribute, if any.
 
-
-		dialog.innerHTML = `
+    dialog.innerHTML = `
     <form method="dialog">
-      <img src="${fullImageLink}" loading="lazy">
-      <p>${description}</p>
+      <img id="dialogImage" src="${fullImageLink}" loading="lazy">
+      <p id="dialogDescription">${description}</p>
       <footer>
         <button type="submit">Close</button>
       </footer>
     </form>
-		`;
-		parent.parentNode.insertBefore(dialog, parent.nextSibling);
+    `;
+    parent.parentNode.insertBefore(dialog, parent.nextSibling);
 
-		img.addEventListener('click', e => {
-			e.preventDefault();
-			dialog.showModal();
-		});
-		
-	}
+    img.addEventListener('click', e => {
+        e.preventDefault();
+        dialog.showModal();
+
+        // Get the naturalWidth of the linked image and set it to the width of the <p>
+        let dialogImage = dialog.querySelector('#dialogImage');
+        let dialogDescription = dialog.querySelector('#dialogDescription');
+        dialogImage.onload = function() {
+            dialogDescription.style.maxWidth = this.naturalWidth + 'px';
+        };
+    });
+}
 
 }
 
