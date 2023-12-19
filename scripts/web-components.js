01e2b8987e00fdbb17db03fa42f4d18b17edda5b
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2023-12-19 12:34:48 +0000

    Add social icons to post headers as Web Component

--- scripts/web-components.js
+++ scripts/web-components.js
@@ -1,10 +1,11 @@
 
 // —————————————————————————image dialog modal Web Component—————————————————————————
+
+
 class DialogImage extends HTMLElement {
 	constructor() {
 		super();
 	}
-
 
 	
   connectedCallback() {
@@ -57,20 +58,58 @@ if(!customElements.get('dialog-image')) customElements.define('dialog-image', Di
 
 // —————————————————————————feedback Web Component—————————————————————————
 	
+
 class Feedback extends HTMLElement {
   
   // connect component
   connectedCallback() {
     this.innerHTML = `
-    <div>
     <p>
-    If you have any comments on this article then please get in touch via <a href="https://twitter.com/dbs_sticky" title="My Twitter profile" target="_blank">X</a> or <a href="https://mastodon.social/@DBSSticky#" title="My Mastodon profile" target="_blank">Mastodon</a>.
-  </p>
-  <div>
+      If you have any comments on this article then please get in touch via <a href="https://twitter.com/dbs_sticky" title="My Twitter profile" target="_blank">X</a> or <a href="https://mastodon.social/@DBSSticky#" title="My Mastodon profile" target="_blank">Mastodon</a>.
+    </p>
   `;
   }
-  
 }
-
 // register component
 if(!customElements.get('feedback-contact')) customElements.define('feedback-contact', Feedback);
+
+
+// —————————————————————————social links Web Component—————————————————————————
+	
+
+class Social extends HTMLElement {
+
+  // connect component
+  connectedCallback() {
+
+    // Get the title and URL from the Open Graph tags.
+    let ogTitle = document.querySelector("meta[property=\"og:title\"]").content;
+    let ogUrl = document.querySelector("meta[property=\"og:url\"]").content;
+
+    // Convert the title and URL to URIs.
+    let titleUri = encodeURIComponent(ogTitle);
+    let urlUri = encodeURIComponent(ogUrl)
+
+    // Concatenate the titleURI and UrlUri together into a sharing link for each social network.
+    this.innerHTML = `
+      <a href='http://reddit.com/submit?url=${urlUri}&title=${titleUri}' target='_blank' title='Share this article on Reddit'>
+        <reddit-icon></reddit-icon>
+      </a>
+      <a href='https://www.facebook.com/sharer/sharer.php?u=${urlUri}' target='_blank' title='Share this article on Facebook'>
+        <fb-icon></fb-icon>
+      </a>
+      <a href='https://x.com/intent/tweet?url=${urlUri}&text=${titleUri}' target='_blank' title='Share this article on X'>
+        <x-icon></x-icon>
+      </a>
+      <a href='http://www.linkedin.com/shareArticle?mini=true&url=${urlUri}&title=${titleUri}' target='_blank' title='Share this article on LinkedIn'>
+        <linkedin-icon></linkedin-icon>
+      </a>
+      <a href='mailto:?subject=${titleUri}&body=${urlUri}' target='_blank' title='Share this article on Email'>
+        <email-icon></email-icon>
+      </a>
+    `;
+  }
+}
+  
+// register component
+if(!customElements.get('social-icons')) customElements.define('social-icons', Social);
